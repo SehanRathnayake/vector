52c7d859a13306ae5e31a5f6e200d38a1be5d8f0
anjana <anjanagunasekara.12@cse.mrt.ac.lk>
2016-08-07 15:34:29 +0530

    js files configured

---
+++ src/main/webapp/resources/js/login.js
@@ -0,0 +1,19 @@
+/**
+    * @author MaN
+    on 8/7/2016.
+    */
+VECTOR.namespace("module.login");
+
+VECTOR.module.login = function() {
+    var idPrefix = "#L_";
+
+    return {
+        init : function() {
+
+        }
+    }
+}();
+
+$(function() {
+    VECTOR.module.login.init();
+});
