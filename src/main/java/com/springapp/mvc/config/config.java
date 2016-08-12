0c178dccd3a196369a008b653e476ecbc778a46b
Sehan Rathnayake <Sehan Rathnayake>
2016-08-12 13:49:00 +0530

    quick fix

--- src/main/java/com/springapp/mvc/config/config.java
+++ src/main/java/com/springapp/mvc/config/config.java
@@ -1,4 +1,4 @@
-package com.springapp.mvc;
+package com.springapp.mvc.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
